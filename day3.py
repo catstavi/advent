d33a409e36019c6240fd4ea9cbaacb97f84e40f4
Cate Uselton <cathode.use@gmail.com>
2016-12-03 16:18:41 -0800

    day3 part two

--- day3.py
+++ day3.py
@@ -1,11 +1,29 @@
-def run():
+def run_rows():
     with open('day3_input.txt') as f:
         triangles = [line.strip().split() for line in f]
-        valid_triangles = [t for t in triangles if valid_triangle(t)]
-        print valid_triangles
-        print len(valid_triangles)
+        print count_valid_triangles(triangles)
         # print "checking on possible triangle: {}".format(tri_sides)
         # if not valid_triangle(tri_sides):
+
+def run_columns():
+    columns = [[], [], []]
+    with open('day3_input.txt') as f:
+        for line in f:
+            sides = line.strip().split()
+            for side, column in zip(sides, columns):
+                add_side_to_col(side, column)
+    triangles = [t for col in columns for t in col]
+    print count_valid_triangles(triangles)
+
+def count_valid_triangles(triangles):
+     valid_triangles = [t for t in triangles if valid_triangle(t)]
+     return len(valid_triangles)
+
+def add_side_to_col(side, col):
+    if len(col) > 0 and len(col[-1]) < 3:
+        col[-1].append(side)
+    else:
+        col.append([side])
 
 def valid_triangle(sides):
     [s1, s2, s3] = [ int(s) for s in sides ]
@@ -14,4 +32,4 @@ def valid_triangle(sides):
     else:
         return False
 
-run()
+run_columns()
