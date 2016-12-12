c3c1ea8e0078b9321af957961f77915b414c1f6e
Cate Uselton <cathode.use@gmail.com>
2016-12-11 22:43:55 -0800

    day five part 2

--- elixir/lib/day5.ex
+++ elixir/lib/day5.ex
@@ -3,17 +3,17 @@ defmodule Day5 do
 
   def find_relevant_hash(door_id, starting_index) do
     hash = make_hash(door_id, starting_index)
-    # if rem(starting_index, 1000000) == 0 do
-    #   Logger.info("on index #{starting_index}")
-    # end
+    if rem(starting_index, 1000000) == 0 do
+      Logger.info("on index #{starting_index}")
+    end
     if String.slice(hash, 0..4) == "00000" do
-      { String.at(hash, 5), starting_index }
+      { hash, starting_index }
     else
       find_relevant_hash(door_id, starting_index + 1)
     end
   end
 
-  def get_code(door_id, starting_index, code) do
+  def get_code(door_id, starting_index \\ 0, code \\ []) do
     if length(code) == 8 do
       final_code = code
       |> Enum.reverse
@@ -22,12 +22,60 @@ defmodule Day5 do
       IO.puts(final_code)
       final_code
     else
-      { code_number, new_index } = find_relevant_hash(door_id, starting_index)
+      { hash, new_index } = find_relevant_hash(door_id, starting_index)
+      code_number = String.at(hash, 5)
       # Logger.info("found code_number: #{code_number}")
       # Logger.info("found new_index: #{new_index}")
       # Logger.info("passing new code as: #{[code_number | code]}")
       get_code(door_id, new_index + 1, [code_number | code] )
     end
+  end
+
+  def get_code_v2(
+    door_id,
+    starting_index \\ 0,
+    code \\ ["", "", "", "", "", "", "", ""]
+  ) do
+    if not Enum.member?(code, "") do
+      final_code = Enum.join(code, "")
+      IO.puts(final_code)
+      final_code
+    else
+      { hash, new_index } = find_relevant_hash(door_id, starting_index)
+      case Integer.parse(String.at(hash, 5)) do
+        {placement_index, "" } ->
+          if Enum.member?(unfilled_indexes(code), placement_index) do
+            code_element = String.at(hash, 6)
+            new_code = List.insert_at(
+              List.delete_at(code, placement_index),
+              placement_index,
+              code_element
+            )
+            get_code_v2(door_id, new_index + 1, new_code)
+          else
+            get_code_v2(door_id, new_index + 1, code)
+          end
+        :error -> get_code_v2(door_id, new_index + 1, code)
+      end
+    end
+  end
+
+# in retrospect, this is kind of a silly way of doing this
+# if I did it again I would probably just:
+  # check that the number is less than the len -1
+  # get the the value at the index, proceeding only if it's empty
+
+
+  # returns an array of all indexes that still need to be filled
+  def unfilled_indexes(code_array) do
+    indexed_codes = Enum.with_index(code_array)
+    { _, indexes } = Enum.map_reduce(indexed_codes, [], fn(element, indexes) ->
+      case element do
+        {"", idx} -> {element, [idx | indexes]}
+        _ -> {element, indexes}
+      end
+    end)
+    indexes
   end
 
   defp make_hash(door_id, index) do
--- elixir/test/day5_test.exs
+++ elixir/test/day5_test.exs
@@ -3,21 +3,39 @@ defmodule AdventTest.Day5 do
   use ExUnit.Case, async: true
   import Day5
 
-  test "can find the first index that makes 00000 hash" do
-    door_id = "abc"
-    {code_number , index } = Day5.find_relevant_hash(door_id, 0)
-    assert index == 3231929
-    assert code_number == "1"
-  end
+  # test "can find the first index that makes 00000 hash" do
+  #   door_id = "abc"
+  #   {code_number , index } = Day5.find_relevant_hash(door_id, 0)
+  #   assert index == 3231929
+  #   assert code_number == "1"
+  # end
+  #
+  # test "gets the correct code for door_id abc" do
+  #   code = get_code("abc")
+  #   assert code == "18f47a30"
+  # end
+  #
+  # test "find the answer" do
+  #   code = get_code("abbhdwsy")
+  #   assert code == "801b56a7"
+  # end
 
-  test "gets the correct code for door_id abc" do
-    code = get_code("abc", 0, [])
-    assert code == "18f47a30"
-  end
+  # test "get unfilled indexes" do
+  #   example_code = ["a", "", "", "", "", "", "", ""]
+  #   indexes = Enum.sort(unfilled_indexes(example_code))
+  #   assert indexes == [1,2,3,4,5,6,7]
+  #   example_code = ["a", "b", "c", "", "", "", "", "d"]
+  #   indexes = Enum.sort(unfilled_indexes(example_code))
+  #   assert indexes == [3,4,5,6]
+  # end
+  #
+  # test "get_code v2 gets the correct code for door_id abc" do
+  #   code = get_code_v2("abc")
+  #   assert code == "05ace8e3"
+  # end
 
-  test "find the answer" do
-    code = get_code("abbhdwsy", 0, [])
-    assert code == "801b56a7"
+  test "gets answer" do
+    code = get_code_v2("abbhdwsy")
   end
 
 end
